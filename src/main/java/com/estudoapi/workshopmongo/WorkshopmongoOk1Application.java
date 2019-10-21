ff78b338397d244856e276f5ef0e5c4c76271d73
JoaoVictorPereiraTeixeira <joaovictorteixeira9819@gmail.com>
2019-10-21 14:47:36 -0300

    Primeiro commit - projeto criado

---
+++ src/main/java/com/estudoapi/workshopmongo/WorkshopmongoOk1Application.java
@@ -0,0 +1,13 @@
+package com.estudoapi.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopmongoOk1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopmongoOk1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/estudoapi/workshopmongo/WorkshopmongoOk1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudoapi.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoOk1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
