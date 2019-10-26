202b07941add256cdd53126534bdfd6117c38510
JoaoVictorPereiraTeixeira <joaovictorteixeira9819@gmail.com>
2019-10-26 20:33:06 -0300

    Criando entity Post com User aninhadas

--- src/main/java/com/estudoapi/workshopmongo/WorkshopmongoApplication.java
+++ src/main/java/com/estudoapi/workshopmongo/WorkshopmongoApplication.java
@@ -2,12 +2,13 @@ package com.estudoapi.workshopmongo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class WorkshopmongoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WorkshopmongoApplication.class, args);
+		ConfigurableApplicationContext run = SpringApplication.run(WorkshopmongoApplication.class, args);
 	}
 
 }
--- src/main/java/com/estudoapi/workshopmongo/resources/UserResource.java
+++ src/main/java/com/estudoapi/workshopmongo/resources/UserResource.java
@@ -48,7 +48,16 @@ public class UserResource {
 	
 	@RequestMapping(value = "/{id}",method=RequestMethod.DELETE)
 	public ResponseEntity<Void> delete(@PathVariable String id){
-		service.delete(id);
+		service.delete(id); 
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value = "/{id}",method=RequestMethod.PUT)
+	public ResponseEntity<Void> put (@RequestBody UserDTO objDto, @PathVariable String id){
+		User obj = service.fromDTO(objDto);
+		obj.setId(id);
+		obj = service.update(obj);
+		
 		return ResponseEntity.noContent().build();
 	}
 	
