e7e3d6d3aecf56ca273162b4d9e9e79503df0a8e
JoaoVictorPereiraTeixeira <joaovictorteixeira9819@gmail.com>
2019-10-27 12:05:18 -0300

    Consulta simples com query methods

--- src/main/java/com/estudoapi/workshopmongo/services/PostService.java
+++ src/main/java/com/estudoapi/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.estudoapi.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,5 +24,8 @@ public class PostService {
 		return post.get();
 	}
 
+	public List<Post> findByTitle(String txt){
+		return repo.findByTitleContainingIgnoreCase(txt);
+	}
 
 }
